41a40bc76eb240aae0d4aabdc92d97fa27ba1ebf
cronus712 <rayengrid@gmail.com>
2022-12-26 01:00:13 +0100

    products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -22,6 +23,8 @@ class HomeController extends Controller
    //  }
 
     public function index() {
-       return view('home.home');
+       
+       $product = Product::where('trending','on')->take(9)->get();
+       return view('home.home', compact('product'));
      }
 }
